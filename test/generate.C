695f7257f10567acbd9cd8fe2b8dfd0506f41b42
Paul Seyfert <paul.seyfert@mib.infn.it>
2015-09-25 17:40:28 +0200

    generate sane output files

--- test/generate.C
+++ test/generate.C
@@ -1,10 +1,5 @@
 int generate() {
   TFile* orgfile = TFile::Open("org.root");
-  TFile* uncompressed = new TFile("uncompressed.root","recreate","",ROOT::CompressionSettings(ROOT::kZLIB,0));
-  vector<TFile*> zlibs;
-  for (int i = 1; i < 10 ; ++i) zlibs.push_back(new TFile(Form("zlib%d.root",i),"recreate","",ROOT::CompressionSettings(ROOT::kZLIB,i)));
-  vector<TFile*> lzmas;
-  for (int i = 1; i < 10 ; ++i) lzmas.push_back(new TFile(Form("lzma%d.root",i),"recreate","",ROOT::CompressionSettings(ROOT::kLZMA,i)));
   TList* thelist = orgfile->GetListOfKeys();
   TObjLink* thelink = thelist->FirstLink();
   while (thelink) {
@@ -12,21 +7,22 @@ int generate() {
       TTree* t;
       orgfile->GetObject(thelink->GetObject()->GetName(),t);
       t->MakeClass("parent");
-      uncompressed->WriteTObject(t->CloneTree(-1),0,"WriteDelete");
+      TFile* uncompressed = new TFile("uncompressed.root","recreate","",ROOT::CompressionSettings(ROOT::kZLIB,0));
+      uncompressed->WriteTObject(t->CloneTree(-1));
       uncompressed->Close();
       delete uncompressed;
       uncompressed=NULL;
-      for (auto pointer : zlibs) {
-        pointer->WriteTObject(t->CloneTree(-1),0,"WriteDelete");
+      for (int i = 1 ; i < 10 ; ++i) {
+        TFile* pointer = new TFile(Form("zlib%d.root",i),"recreate","",ROOT::CompressionSettings(ROOT::kZLIB,i));
+        pointer->WriteTObject(t->CloneTree(-1));
         pointer->Close();
         delete pointer;
-        pointer=NULL;
       }
-      for (auto pointer : lzmas) {
-        pointer->WriteTObject(t->CloneTree(-1),0,"WriteDelete");
+      for (int i = 1 ; i < 10 ; ++i) {
+        TFile* pointer = new TFile(Form("lzma%d.root",i),"recreate","",ROOT::CompressionSettings(ROOT::kLZMA,i));
+        pointer->WriteTObject(t->CloneTree(-1));
         pointer->Close();
         delete pointer;
-        pointer=NULL;
       }
       return 0;
       break;
